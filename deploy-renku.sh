69229e34adcacec1896bff79c22c37de7998da77
Jirka Kremser <jkremser@redhat.com>
2019-06-25 17:03:17 +0200

    update on the automation script, nginx is now being deployed w/ oc new-app and s2i

--- deploy-renku.sh
+++ deploy-renku.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+OC_CLUSTER_ID="brno-8647"
+HELM_VERSION=${HELM_VERSION:-"v2.11.0"}
+export TILLER_NAMESPACE=${TILLER_NAMESPACE:-"tiller"}
+ACME_NAMESPACE=${ACME_NAMESPACE:-"acme"}
+RENKU_NAMESPACE=${RENKU_NAMESPACE:-"renku"}
+HOST=${HOST:-"renku.apps.cluster-$OC_CLUSTER_ID.$OC_CLUSTER_ID.openshiftworkshop.com"}
 
-HELM_VERSION=${HELM_VERSION:-v2.11.0}
-export TILLER_NAMESPACE=${TILLER_NAMESPACE:-tiller}
-ACME_NAMESPACE=${ACME_NAMESPACE:-acme}
-RENKU_NAMESPACE=${RENKU_NAMESPACE:-renku}
 
 #rbac openshift
-oc replace --filename ./htpass-secret.yaml
+# oc replace --filename ./htpass-secret.yaml
 
 # tiller
 oc new-project ${TILLER_NAMESPACE} &> /dev/null && echo "namespace ${TILLER_NAMESPACE} created"
@@ -17,16 +19,24 @@ oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:tille
 
 # certs
 oc new-project ${ACME_NAMESPACE} &> /dev/null && echo "namespace ${ACME_NAMESPACE} created"
-oc apply -f acme -n ${ACME_NAMESPACE}
+oc apply -f acme/ -n ${ACME_NAMESPACE}
 oc adm policy add-cluster-role-to-user openshift-acme -z openshift-acme -n ${ACME_NAMESPACE}
 
 # renku
 oc new-project ${RENKU_NAMESPACE} &> /dev/null && echo "namespace ${RENKU_NAMESPACE} created"
 oc adm policy add-scc-to-user anyuid -z default -z hub
+
+# process the value file
+sed "s;#@renku@#;$HOST;g" values-template.yaml > values.yaml
 helm upgrade --install renku --namespace ${RENKU_NAMESPACE} -f values.yaml renku-0.3.2.tgz
+rm values.yaml || true
 
 # nginx
-# deploy.. 
-#registry.access.redhat.com/rhscl/nginx-112-rhel7
+oc new-app nginx:1.12~https://github.com/jkremser/renku-openshift.git --context-dir=nginx --name=nginx
+sleep 60 # meh
+sed "s;#@renku@#;$HOST;g" nginx-route-template.yaml > nginx-route.yaml
+oc apply -f ./nginx-route.yaml
+rm nginx-route.yaml || true
+oc patch route renku -p '{"metadata":{"annotations":{"kubernetes.io/tls-acme":"true"}}}'
 
-#oc patch route nginx-ex -p '{"metadata":{"annotations":{"kubernetes.io/tls-acme":"true"}}}'
+# readiness probe for gw to true
